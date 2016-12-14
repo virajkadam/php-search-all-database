a8d395ea8f466e741c6f4057dc87d97e61b62c1d
Viraj Kadam <kadam.virajkadam@gmail.com>
2016-12-14 11:11:26 +0530

    Update php-search-all-database.php

--- php-search-all-database.php
+++ php-search-all-database.php
@@ -1,9 +1,8 @@
 <?php
 
-$search_keyword = "keyword"; 					// search Keyword
-$table_array = ['',''];							// table names in array format
-php_search_all_database($search_keyword, $table_array); // call this Awesomme function
-
+$search_keyword = "keyword"; 								// search Keyword
+$table_array = ['',''];										// table names in array format
+php_search_all_database($search_keyword, $table_array); 	// call this Awesomme function
 
 function php_search_all_database($search_keyword,$table_array){
 
@@ -15,8 +14,8 @@ function php_search_all_database($search_keyword,$table_array){
 	$db_database_name = ''; 					// database name
 
 	$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database_name);
-		// Check connection
-		if(mysqli_connect_errno()){
+	
+		if(mysqli_connect_errno()){									// Check connection
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
 
@@ -24,8 +23,8 @@ function php_search_all_database($search_keyword,$table_array){
 		echo "<b>Given Keyword :</b> ".$search_keyword . '<br>';
 		echo "<b>Given tables :</b> ".implode($rt,', ') . '<br>';
 
-		if(count($rt) > 0){ 							// check weather table column is not empty
-			foreach($rt as $r){ 						// iterate column name table
+		if(count($rt) > 0){ 										// check weather table column is not empty
+			foreach($rt as $r){ 									// iterate column name table
 				$table = $r;
 				$sql2 = "SHOW COLUMNS FROM ".$table;
 				$rs2 = $conn->query($sql2);
@@ -38,7 +37,7 @@ function php_search_all_database($search_keyword,$table_array){
 						$sql_search = "SELECT * FROM " . $table . " WHERE " . $sql_search_fields;
 						$rs3 = $conn->query($sql_search);
 
-						if($rs3->num_rows > 0){ 							// check weather 'keyword' found or not
+						if($rs3->num_rows > 0){ 								// check weather 'keyword' found or not
 							
 							echo "<ul><u>Table :" . $table . '</u>';
 							while($r3 = $rs3->fetch_array()){ 			    	// fetch result from respective data
@@ -53,11 +52,11 @@ function php_search_all_database($search_keyword,$table_array){
 				 	}
 				}
 
-				if($rs3->num_rows > 0){ 							// check weather total count is not zero
-					echo "&rarr; Result Count :" . $count;			// display total result count
+				if($rs3->num_rows > 0){ 						// check weather total count is not zero
+					echo "&rarr; Result Count :" . $count;		// display total result count
 				}
-				$rs3->close(); 							// reset resultset (check keyword found or not)
-				$rs2->close(); 							// reset resultset (check given column names found or not)
+				$rs3->close(); 									// reset resultset (check keyword found or not)
+				$rs2->close(); 									// reset resultset (check given column names found or not)
 			}
 		}
 }
