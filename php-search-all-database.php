3af28fa7f4d2fb6df5243922cd43506a40ccaf8c
Viraj Kadam <hivirajkadam@gmail.com>
2022-07-28 01:12:44 +0530

    Clean comments, descriptiive varibale names

--- php-search-all-database.php
+++ php-search-all-database.php
@@ -1,67 +1,79 @@
 <?php
-$search_keyword = "KEYWORD";			// search Keyword
-$table_ass_array = array( 
-			'TABLE NAME 1' => array( 			// TABLENAME 1 to search in
-							'COLUMNNAME_A',		// Column Name A to search in
-							'COLUMNNAME_B'		// Column Name B to search in
-						),
+
+$search_keyword = "KEYWORD";						// Enter keyword to search
+
+$table_associative_array = array( 
+			'TABLE NAME 1' => array(			// TABLENAME 1 to search in
+				'columnN_NAME_A',			// column Name A to search in
+				'columnN_NAME_B'			// column Name B to search in
+			),
 			'TABLE NAME 2' => array(			// TABLENAME 2 to search in
-							'COLUMNNAME_A',		// Column Name A to search in
-							'COLUMNNAME_B'		// Column Name B to search in
-						)
-			);
+				'columnN_NAME_A',			// column Name A to search in
+				'columnN_NAME_B'			// column Name B to search in
+			)
+		);
 
-php_search_all_database($search_keyword, $table_ass_array);		// call this Awesomme function
 
-function php_search_all_database($search_keyword,$table_ass_array){
+php_search_all_database( $search_keyword, $table_associative_array );	// call this Awesome function to run script
 
-	global $conn;							// declared global variable conn
 
-	$db_hostname = 'DATABASE HOST NAME'; 				// database hostname (default value: localhost)
-	$db_username = 'DATABASE USERNAME'; 				// database username (default value: root)
-	$db_password = 'DATABASE PASSWORD'; 				// database password (default value: password)
-	$db_database_name = 'DATABASE NAME'; 				// database name
 
-	$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database_name);	// create connection
+function php_search_all_database($search_keyword,$table_associative_array){
 
-		if(mysqli_connect_errno()){		// Check connection
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	global $conn;		// Declared global variable to store database connection
+
+	$db_hostname = 'DATABASE HOST NAME';		// Database hostname (default value: localhost)
+	$db_username = 'DATABASE USERNAME'; 		// Database username (default value: root)
+	$db_password = 'DATABASE PASSWORD'; 		// Database password
+	$db_database_name = 'DATABASE NAME'; 		// Database name
+
+	$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database_name);	// Establish Database Connection
+
+		if(mysqli_connect_errno()){		// Check if database connection is ok
+			echo "Failed to connect to MySQL: ".mysqli_connect_error();
 		}
 
-		$rt = $table_ass_array;
-		echo "<b>Given Keyword :</b> ".$search_keyword . '<br>';
-		echo "<b>Given tables :</b> " . implode($rt,', ') . '<br>';
 
-		if(count($rt) > 0){ 							// check weather table column is not empty
-			foreach($rt as $k=>$v){ 						// iterate column name table
-				echo $table = $k;
-				echo $v;
-				foreach($v as $r2){	// fetch data from respective column name
+		echo "<b>Given Keyword :</b> ".$search_keyword.'<br>';
+		echo "<b>Given tables :</b> ".implode($table_associative_array,', ').'<br>';
 
-					$colum = $r2;	//[0];
-					$sql_search_fields = $colum . " LIKE ('%" . $search_keyword . "%')";
-					$sql_search = "SELECT * FROM " . $table . " WHERE " . $sql_search_fields;
-					$rs3 = $conn->query($sql_search);
 
-					if($rs3->num_rows > 0){ 				// check weather 'keyword' found or not
+		if(count($table_associative_array) > 0){					// Check weather array of tables names and column names is not empty
 
-						echo "<ul><u>Table :" . $table . '</u>';
-						while($r3 = $rs3->fetch_array()){ 	// fetch result from respective data
+			foreach($table_associative_array AS $table_name => $columnn_name){ 	// Iterate through array of table names
+
+				echo $table_name;						// Name of table
+				echo "<br>";
+				echo $columnn_name;						// Name of Column in this table
+				echo "<br><br>";
+
+				foreach($columnn_name AS $column){				// Fetch data from array of column names
+
+					$db_search_result_fields = $column." LIKE ('%".$search_keyword."%')";		// We have used wildcards as an example, You can replace as per your need
+					$db_search_result = $conn->query("SELECT * FROM ".$table." WHERE ".$db_search_result_fields);
+
+					if($db_search_result->num_rows > 0){ 			// Check weather 'keyword' found or not
+
+						echo "<ul><u>Table :".$table.'</u>';
+
+						while( $row = $db_search_result->fetch_array() ){ 	// Fetch final result from records found
 							$count++;
-							echo "<li> Column Name : " . $colum . "</li>";
-							echo "<li> Row  : " . $r3['ROW ID'] . "</li>";	// primary key column name
-							echo "<li> Value : " . $r3[$colum] . "</li><br>";
-						
-						}	// while loop close
+							echo "<li>Column Name: ".$column."</li>";	// Respective column Name
+							echo "<li>Row: ".$row['ROW ID']."</li>";	// Primary key of respective table name, For example id/rowId
+							echo "<li>Value: ".$row[$column]."</li><br>";	// Data stored in respective columns. i.e. The actual keyword we found
 
-					}	// table data count close
-					echo "</ul>";
+						}	// End of while loop of final result 
 
-				}	//foreach close
+						echo "</ul>";
 
-				echo $table." Searching End's Here<hr>";
+					}	// End of if condition to check table data count
 
-			}	//foreach close
+				}	// End of foreach of data fetching of every column
 
-		}	// Table count close
-}
+				echo $table." search End's Here<hr><br>";
+
+			}	// End of foreach of data fetching of every table
+
+		}	// End of if condition to check empty input data
+
+	}	// Awesome function ends
